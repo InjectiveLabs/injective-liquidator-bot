20d7c11d26aa9789dccbabfcdf2fb08c22adbeb7
abel <abarmoa@gmail.com>
2024-02-02 23:02:02 -0300

    (feat) Added unit tests for the liquidate position message creation for delegated and not delegated accounts

---
+++ internal/pkg/service/service_test.go
@@ -0,0 +1,157 @@
+package service
+
+import (
+	"context"
+	exchangetypes "github.com/InjectiveLabs/sdk-go/chain/exchange/types"
+	"github.com/InjectiveLabs/sdk-go/client/chain"
+	"github.com/InjectiveLabs/sdk-go/client/exchange"
+	derivativeExchangePB "github.com/InjectiveLabs/sdk-go/exchange/derivative_exchange_rpc/pb"
+	spotExchangePB "github.com/InjectiveLabs/sdk-go/exchange/spot_exchange_rpc/pb"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	eth "github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLiquidatePositionMessageWhenNotUsingDelegatedAccount(t *testing.T) {
+	granterPublicAddress := ""
+	granteePublicAddress := "inj14au322k9munkmx5wrchz9q30juf5wjgz2cfqku"
+	granteeSubaccountID := eth.HexToHash("0x00606da8ef76ca9c36616fa576d1c053bb0f7eb2000000000000000000000000")
+	// granterSubaccountID := eth.HexToHash("0xbdaedec95d563fb05240d6e01821008454c24c36000000000000000000000000")
+	granterSubaccountID := eth.HexToHash("")
+
+	mockChain := LocalMockChainClient{}
+	mockExchange := exchange.MockExchangeClient{}
+
+	address, _ := types.AccAddressFromBech32(granteePublicAddress)
+	mockChain.FromAddresses = append(mockChain.FromAddresses, address)
+
+	var derivativeMarketInfos []*derivativeExchangePB.DerivativeMarketInfo
+	btcUsdtDerivativeMarketInfo := createBTCUSDTDerivativeMarketInfo()
+
+	derivativeMarketInfos = append(derivativeMarketInfos, btcUsdtDerivativeMarketInfo)
+	mockExchange.SpotMarketsResponses = append(mockExchange.SpotMarketsResponses, &spotExchangePB.MarketsResponse{
+		Markets: []*spotExchangePB.SpotMarketInfo{},
+	})
+	mockExchange.DerivativeMarketsResponses = append(mockExchange.DerivativeMarketsResponses, &derivativeExchangePB.MarketsResponse{
+		Markets: derivativeMarketInfos,
+	})
+
+	ctx := context.Background()
+	marketAssistant, err := chain.NewMarketsAssistantInitializedFromChain(ctx, &mockExchange)
+
+	assert.NoError(t, err)
+
+	liquidatorService := liquidatorSvc{
+		chainClient:          &mockChain,
+		exchangeClient:       &mockExchange,
+		marketsAssistant:     marketAssistant,
+		marketID:             btcUsdtDerivativeMarketInfo.MarketId,
+		subaccountID:         granteeSubaccountID,
+		granterPublicAddress: granterPublicAddress,
+		granterSubaccountID:  granterSubaccountID,
+	}
+
+	market := marketAssistant.AllDerivativeMarkets()[btcUsdtDerivativeMarketInfo.MarketId]
+	position := derivativeExchangePB.DerivativePosition{
+		MarketId:     market.Id,
+		SubaccountId: "positionSubaccountID",
+		Direction:    "long",
+		Quantity:     "1",
+		MarkPrice:    "3400000000",
+	}
+
+	message := liquidatorService.createLiquidationMessage(&position, market)
+	liquidationMessage := message.(*exchangetypes.MsgLiquidatePosition)
+
+	assert.Equal(t, granteePublicAddress, liquidationMessage.Sender)
+	assert.Equal(t, position.SubaccountId, liquidationMessage.SubaccountId)
+	assert.Equal(t, market.Id, position.MarketId)
+	assert.Equal(t, market.Id, liquidationMessage.Order.GetMarketId())
+	assert.Equal(t, granteeSubaccountID.String(), liquidationMessage.Order.OrderInfo.SubaccountId)
+	assert.Equal(t, granteePublicAddress, liquidationMessage.Order.OrderInfo.FeeRecipient)
+	assert.Equal(t, types.MustNewDecFromStr(position.MarkPrice), liquidationMessage.Order.OrderInfo.Price)
+	assert.Equal(t, types.MustNewDecFromStr(position.Quantity), liquidationMessage.Order.OrderInfo.Quantity)
+	assert.NotEqual(t, "", liquidationMessage.Order.OrderInfo.GetCid())
+	assert.Equal(t, exchangetypes.OrderType_BUY, liquidationMessage.Order.OrderType)
+	assert.Equal(
+		t,
+		types.MustNewDecFromStr(position.Quantity).Mul(types.MustNewDecFromStr(position.MarkPrice)),
+		liquidationMessage.Order.GetMargin(),
+	)
+}
+
+func TestLiquidatePositionMessageWhenUsingDelegatedAccount(t *testing.T) {
+	granterPublicAddress := "granterPublicAddress"
+	granteePublicAddress := "inj14au322k9munkmx5wrchz9q30juf5wjgz2cfqku"
+	granteeSubaccountID := eth.HexToHash("0x00606da8ef76ca9c36616fa576d1c053bb0f7eb2000000000000000000000000")
+	granterSubaccountID := eth.HexToHash("0xbdaedec95d563fb05240d6e01821008454c24c36000000000000000000000000")
+
+	mockChain := LocalMockChainClient{}
+	mockExchange := exchange.MockExchangeClient{}
+
+	address, _ := types.AccAddressFromBech32(granteePublicAddress)
+	mockChain.FromAddresses = append(mockChain.FromAddresses, address)
+
+	var derivativeMarketInfos []*derivativeExchangePB.DerivativeMarketInfo
+	btcUsdtDerivativeMarketInfo := createBTCUSDTDerivativeMarketInfo()
+
+	derivativeMarketInfos = append(derivativeMarketInfos, btcUsdtDerivativeMarketInfo)
+	mockExchange.SpotMarketsResponses = append(mockExchange.SpotMarketsResponses, &spotExchangePB.MarketsResponse{
+		Markets: []*spotExchangePB.SpotMarketInfo{},
+	})
+	mockExchange.DerivativeMarketsResponses = append(mockExchange.DerivativeMarketsResponses, &derivativeExchangePB.MarketsResponse{
+		Markets: derivativeMarketInfos,
+	})
+
+	ctx := context.Background()
+	marketAssistant, err := chain.NewMarketsAssistantInitializedFromChain(ctx, &mockExchange)
+
+	assert.NoError(t, err)
+
+	liquidatorService := liquidatorSvc{
+		chainClient:          &mockChain,
+		exchangeClient:       &mockExchange,
+		marketsAssistant:     marketAssistant,
+		marketID:             btcUsdtDerivativeMarketInfo.MarketId,
+		subaccountID:         granteeSubaccountID,
+		granterPublicAddress: granterPublicAddress,
+		granterSubaccountID:  granterSubaccountID,
+	}
+
+	market := marketAssistant.AllDerivativeMarkets()[btcUsdtDerivativeMarketInfo.MarketId]
+	position := derivativeExchangePB.DerivativePosition{
+		MarketId:     market.Id,
+		SubaccountId: "positionSubaccountID",
+		Direction:    "long",
+		Quantity:     "1",
+		MarkPrice:    "3400000000",
+	}
+
+	message := liquidatorService.createLiquidationMessage(&position, market)
+	execMessage := message.(*authz.MsgExec)
+
+	assert.Equal(t, granteePublicAddress, execMessage.Grantee)
+
+	liquidationMessage := exchangetypes.MsgLiquidatePosition{}
+	unmarshallError := liquidationMessage.Unmarshal(execMessage.Msgs[0].GetValue())
+
+	assert.NoError(t, unmarshallError)
+
+	assert.Equal(t, granterPublicAddress, liquidationMessage.Sender)
+	assert.Equal(t, position.SubaccountId, liquidationMessage.SubaccountId)
+	assert.Equal(t, market.Id, position.MarketId)
+	assert.Equal(t, market.Id, liquidationMessage.Order.GetMarketId())
+	assert.Equal(t, granterSubaccountID.String(), liquidationMessage.Order.OrderInfo.SubaccountId)
+	assert.Equal(t, granterPublicAddress, liquidationMessage.Order.OrderInfo.FeeRecipient)
+	assert.Equal(t, types.MustNewDecFromStr(position.MarkPrice), liquidationMessage.Order.OrderInfo.Price)
+	assert.Equal(t, types.MustNewDecFromStr(position.Quantity), liquidationMessage.Order.OrderInfo.Quantity)
+	assert.NotEqual(t, "", liquidationMessage.Order.OrderInfo.GetCid())
+	assert.Equal(t, exchangetypes.OrderType_BUY, liquidationMessage.Order.OrderType)
+	assert.Equal(
+		t,
+		types.MustNewDecFromStr(position.Quantity).Mul(types.MustNewDecFromStr(position.MarkPrice)),
+		liquidationMessage.Order.GetMargin(),
+	)
+}
---
+++ internal/pkg/service/service_test_support.go
@@ -0,0 +1,110 @@
+package service
+
+import (
+	exchangetypes "github.com/InjectiveLabs/sdk-go/chain/exchange/types"
+	"github.com/InjectiveLabs/sdk-go/client/chain"
+	derivativeExchangePB "github.com/InjectiveLabs/sdk-go/exchange/derivative_exchange_rpc/pb"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	eth "github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
+)
+
+func createUSDTPerpTokenMeta() derivativeExchangePB.TokenMeta {
+	return derivativeExchangePB.TokenMeta{
+		Name:      "Tether",
+		Address:   "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+		Symbol:    "USDTPerp",
+		Logo:      "https://static.alchemyapi.io/images/assets/825.png",
+		Decimals:  6,
+		UpdatedAt: 1683929869866,
+	}
+}
+
+func createBTCUSDTDerivativeMarketInfo() *derivativeExchangePB.DerivativeMarketInfo {
+	usdtPerpTokenMeta := createUSDTPerpTokenMeta()
+
+	perpetualMarketInfo := derivativeExchangePB.PerpetualMarketInfo{
+		HourlyFundingRateCap: "0.0000625",
+		HourlyInterestRate:   "0.00000416666",
+		NextFundingTimestamp: 1684764000,
+		FundingInterval:      3600,
+	}
+
+	perpetualmarketFunding := derivativeExchangePB.PerpetualMarketFunding{
+		CumulativeFunding: "6880500093.266083891331674194",
+		CumulativePrice:   "-0.952642601240470199",
+		LastTimestamp:     1684763442,
+	}
+
+	marketInfo := derivativeExchangePB.DerivativeMarketInfo{
+		MarketId:               "0x4ca0f92fc28be0c9761326016b5a1a2177dd6375558365116b5bdda9abc229ce",
+		MarketStatus:           "active",
+		Ticker:                 "BTC/USDT PERP",
+		OracleBase:             "BTC",
+		OracleQuote:            "USDT",
+		OracleType:             "bandibc",
+		OracleScaleFactor:      6,
+		InitialMarginRatio:     "0.095",
+		MaintenanceMarginRatio: "0.025",
+		QuoteDenom:             "peggy0xdAC17F958D2ee523a2206206994597C13D831ec7",
+		QuoteTokenMeta:         &usdtPerpTokenMeta,
+		MakerFeeRate:           "-0.0001",
+		TakerFeeRate:           "0.001",
+		ServiceProviderFee:     "0.4",
+		IsPerpetual:            true,
+		MinPriceTickSize:       "1000000",
+		MinQuantityTickSize:    "0.0001",
+		PerpetualMarketInfo:    &perpetualMarketInfo,
+		PerpetualMarketFunding: &perpetualmarketFunding,
+	}
+
+	return &marketInfo
+}
+
+type LocalMockChainClient struct {
+	chain.MockChainClient
+	FromAddresses       []sdk.AccAddress
+	BroadcastedMessages []sdk.Msg
+}
+
+func (c *LocalMockChainClient) FromAddress() sdk.AccAddress {
+	address := c.FromAddresses[0]
+	c.FromAddresses = c.FromAddresses[1:len(c.FromAddresses)]
+	return address
+}
+
+func (c *LocalMockChainClient) CreateDerivativeOrder(defaultSubaccountID eth.Hash, d *chain.DerivativeOrderData, marketAssistant chain.MarketsAssistant) *exchangetypes.DerivativeOrder {
+	market, isPresent := marketAssistant.AllDerivativeMarkets()[d.MarketId]
+	if !isPresent {
+		panic(errors.Errorf("Invalid derivative market id %s", d.MarketId))
+	}
+
+	orderSize := market.QuantityToChainFormat(d.Quantity)
+	orderPrice := market.PriceToChainFormat(d.Price)
+	orderMargin := sdk.MustNewDecFromStr("0")
+
+	if !d.IsReduceOnly {
+		orderMargin = market.CalculateMarginInChainFormat(d.Quantity, d.Price, d.Leverage)
+	}
+
+	return &exchangetypes.DerivativeOrder{
+		MarketId:  d.MarketId,
+		OrderType: d.OrderType,
+		Margin:    orderMargin,
+		OrderInfo: exchangetypes.OrderInfo{
+			SubaccountId: defaultSubaccountID.Hex(),
+			FeeRecipient: d.FeeRecipient,
+			Price:        orderPrice,
+			Quantity:     orderSize,
+			Cid:          d.Cid,
+		},
+	}
+}
+
+func (c *LocalMockChainClient) SyncBroadcastMsg(msgs ...sdk.Msg) (*tx.BroadcastTxResponse, error) {
+	for _, msg := range msgs {
+		c.BroadcastedMessages = append(c.BroadcastedMessages, msg)
+	}
+	return &tx.BroadcastTxResponse{}, nil
+}
